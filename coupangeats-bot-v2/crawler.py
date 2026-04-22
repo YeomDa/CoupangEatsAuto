2af5d4b16cf92419147354b44ad1250a119558c3
YeomDa <73528833+YeomDa@users.noreply.github.com>
2026-04-22 17:17:34 +0900

    Update crawler.py

--- coupangeats-bot-v2/crawler.py
+++ coupangeats-bot-v2/crawler.py
@@ -27,8 +27,6 @@ def get_driver() -> webdriver.Chrome:
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
